d112263010460fcd6611f64f3a13340d169bdba7
brahim360 <brahim.real.360@hotmail.fr>
2021-09-29 13:58:12 +0100

    dolar to DINAR

--- src/components/popular/index.js
+++ src/components/popular/index.js
@@ -4,13 +4,13 @@ import './style.css'
 
 
 const Speciality_Dishes = [
-    { img: "images/p-1.jpg", title: "tasty burger",price:"$7 - $10" },
-    { img: "images/p-2.jpg", title: "tasty pizza" ,price:"$5 - $20"},
-    { img: "images/p-3.jpg", title: "cold ice-cream",price:"$6 - $24" },
-    { img: "images/p-4.jpg", title: "cold drinks" ,price:"$7 - $20"},
-    { img: "images/p-5.jpg", title: "tasty sweets",price:"$8 - $23" },
-    { img: "images/p-6.jpg", title: "healty breakfast" ,price:"$3 - $20"},
-    { img: "images/p-3.jpg", title: "cold ice-creamm",price:"$6 - $24" },
+    { img: "images/p-1.jpg", title: "tasty burger",price:"DA7 - DA10" },
+    { img: "images/p-2.jpg", title: "tasty pizza" ,price:"DA5 - DA20"},
+    { img: "images/p-3.jpg", title: "cold ice-cream",price:"DA6 - DA24" },
+    { img: "images/p-4.jpg", title: "cold drinks" ,price:"DA7 - DA20"},
+    { img: "images/p-5.jpg", title: "tasty sweets",price:"DA8 - DA23" },
+    { img: "images/p-6.jpg", title: "healty breakfast" ,price:"DA3 - DA20"},
+    { img: "images/p-3.jpg", title: "cold ice-creamm",price:"DA6 - DA24" },
     
     
   ]
