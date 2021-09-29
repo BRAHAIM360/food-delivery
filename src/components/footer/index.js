7a69b51c9fa0c60bbe7ddd51edb356a0ef85c969
BRAHIM MAHIOUSSI <brahim.real.360@hotmail.fr>
2021-09-29 22:01:52 +0100

    Social Media Link

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,37 +3,42 @@ import "./style.css";
 
 function Footer() {
 
+   function openInNewTab(url) {
+      window.open(url, '_blank').focus();
+     }
+     
+
     return (
       
 <section class="footer">
 
 
 <div class="wrapper share">
-         <div class="button">
+         <div onClick={()=>openInNewTab("https://web.facebook.com/brahim.madrido/")} class="button">
             <div class="icon">
                <i class="fab fa-facebook-f"></i>
             </div>
             <span>Facebook</span>
          </div>
-         <div class="button">
+         <div class="button"  onClick={()=>openInNewTab("https://twitter.com/Aek360")}>
             <div class="icon">
                <i class="fab fa-twitter"></i>
             </div>
             <span>Twitter</span>
          </div>
-         <div class="button">
+         <div class="button"  onClick={()=>openInNewTab("https://www.instagram.com/brahim.360/")}>
             <div class="icon">
                <i class="fab fa-instagram"></i>
             </div>
             <span>Instagram</span>
          </div>
-         <div class="button">
+         <div class="button"  onClick={()=>openInNewTab("https://www.linkedin.com/in/brahim-mahioussi-4b2920111/")}>
             <div class="icon">
-               <i class="fab fa-linkedin"></i>
+               <i class="fab fa-linkedin" ></i>
             </div>
             <span>Linkedin</span>
          </div>
-         <div class="button">
+         <div class="button"  onClick={()=>openInNewTab("https://www.youtube.com/channel/UChzCBkBgOQbyVwNvdvkhUEg")}>
             <div class="icon">
                <i class="fab fa-youtube"></i>
             </div>
