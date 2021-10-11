672dfcbc1aa64eb559207b84f91beaafdf1878e3
BRAHIM MAHIOUSSI <brahim.real.360@hotmail.fr>
2021-10-11 18:43:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
+import "./App.css";
+import NavBar from "./components/navBar";
+import React, { useEffect, useState } from "react";
+import Home from "./components/home";
+import Speciality from "./components/speciality";
+import Popular from "./components/popular";
+import WorkStep from "./components/workStep";
+import Gallery from "./components/gallery";
+import Review from "./components/review";
+import Order from "./components/order";
+import Footer from "./components/footer";
+import Loading from "./components/loading";
+import OnScroll from "./components/onScroll";
+import useScrollPosition from '@react-hook/window-scroll'
 
-import './App.css';
-import NavBar from './components/navBar'
-import React from 'react'
-import Home from './components/home';
-import Speciality from './components/speciality';
-import Popular from './components/popular';
-import WorkStep from './components/workStep';
-import Gallery from './components/gallery';
-import Review from './components/review';
-import Order from './components/order';
-import Footer from './components/footer';
 
 function App() {
-  return (
-    <div>
-<NavBar/>
-<Home/>
-<Speciality/>
-<Popular/>
-<WorkStep/>
-<Gallery/>
-<Review/>
-<Order/>
-<Footer/>
+  const scrollY = useScrollPosition(60 )
+  useEffect(() => {
+    setTimeout(() => {
+      document.querySelector(".loader-container").classList.add("fade-out");
+    }, 4000);
+  }, []);
 
-   </div>
+
+  return (
+   <div>
+          <Loading  />
+        {scrollY>60 ?<OnScroll onScrollVisibal={true}/> :<OnScroll onScrollVisibal={false}/> }
+         <NavBar />
+          <Home />
+          <Speciality />
+          <Popular />
+          <WorkStep />
+          <Gallery />
+          <Review />
+          <Order />
+          <Footer />
+        </div>
+     
   );
 }
 
